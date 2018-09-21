dea0adad5e7803343ef5d300ee63cf7bcd0d2c8f
Max Techera <maxi.techerag@gmail.com>
2018-09-21 00:38:06 -0300

    Make User model migration compatible, add second migration

--- WAC.DataAccess/Migrations/20180921033653_MySecondMigration.Designer.cs
+++ WAC.DataAccess/Migrations/20180921033653_MySecondMigration.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WAC.DataAccess;
 
 namespace WAC.DataAccess.Migrations
 {
     [DbContext(typeof(DomainContext))]
-    partial class DomainContextModelSnapshot : ModelSnapshot
+    [Migration("20180921033653_MySecondMigration")]
+    partial class MySecondMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
